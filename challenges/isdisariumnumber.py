fd8bb6117b66a575a440efda45ea5c549d43de46
charleslando <clando1@mail.yu.edu>
2023-06-04 20:40:33 -0400

    one hard one left

--- challenges/isdisariumnumber.py
+++ challenges/isdisariumnumber.py
@@ -1,14 +1,41 @@
+# is_disarium(75) ➞ False
+# # 7^1 + 5^2 = 7 + 25 = 32
+
+# is_disarium(135) ➞ True
+# # 1^1 + 3^2 + 5^3 = 1 + 9 + 125 = 135
+
+# is_disarium(544) ➞ False
+
+# is_disarium(518) ➞ True
+
+# is_disarium(466) ➞ False
+
+# is_disarium(8) ➞ True
+def is_disarium(n):
+    reverse = reverse_num(n)
+    final = 0
+    counter = 0
+    while reverse > 0:
+        counter += 1
+        final += (reverse % 10) ** counter
+        reverse = reverse // 10
+
+    if(n == final):
+        return True
+    return False
+
+    
+
+
 def reverse_num(num):
     temp = 0
     while num > 0:
-        temp = num/10
-        temp*=10
+        temp = (temp * 10) + (num % 10)
+        num = num // 10
     return temp
 
-#def is_disarium(n):
 
 
-
-print(reverse_num(78126))
-print(reverse_num(7))
-print("hi")
+print(is_disarium(135))
+print(is_disarium(466))
+print(is_disarium(518))
